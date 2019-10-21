ff022ff76f5656afa7844caf4d09c25642aeca7e
Louis <louis.schuck@gmail.com>
2019-10-21 09:42:47 +0200

    Added a function "is_mouth_open"

--- bodyDetection/PLScare/image_treatment.py
+++ bodyDetection/PLScare/image_treatment.py
@@ -2,4 +2,4 @@ import numpy as np
 
 
 def get_average_value(img):
-    return np.average(img[:, :, 0]*0.1140 + img[:, :, 0]*0.5870 + img[:, :, 0]*0.2989)
+    return np.average(img[:, :, 0]*0.1140 + img[:, :, 1]*0.5870 + img[:, :, 2]*0.2989)
