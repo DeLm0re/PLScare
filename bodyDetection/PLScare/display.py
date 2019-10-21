85eb3e4f2ab7c43701ab0c892b608ae0e299ed6a
RomainJacquiez <jacquiez.romain@orange.fr>
2019-10-21 08:54:09 +0200

    Reduce kv file and clean code

--- bodyDetection/PLScare/display.py
+++ bodyDetection/PLScare/display.py
@@ -18,16 +18,38 @@ def run_app():
 
 
 class PlscareApp(App):
+
     def build(self):
         return PlscareLayout()
 
 
 class LogoImg(Image):
-    pass
+
+    def __init__(self, **kwargs):
+        super(LogoImg, self).__init__(**kwargs)
+
+        # param for the image
+        self.source = 'PLScare/assets/images/logo_PLScare.png'
+        self.size_hint = (None, None)
+        self.size_hint = 0.8, 0.8
+        self.pos_hint = {'center_x': 0.5, 'center_y': 0.7}
 
 
 class HubBtn(Button):
-    pass
+
+    def __init__(self, **kwargs):
+        super(HubBtn, self).__init__(**kwargs)
+
+        # param for the button
+        self.text = 'Start recording'
+        self.size_hint = (None, None)
+        self.pos_hint = {'center_x': 0.5, 'center_y': 0.3}
+        self.font_name = 'PLScare/assets/fonts/OpenSans-Bold.ttf'
+        self.font_size = 50
+        self.color = 1, 0, 0, 1
+        self.padding = (30, 10)
+        self.background_normal = ''
+        self.background_down = 'PLScare/assets/images/grey.png'
 
 
 class PlscareLayout(FloatLayout):
@@ -46,7 +68,6 @@ class PlscareLayout(FloatLayout):
         btn_hub.bind(on_press=self.checkout)
 
         # add element in layout
-        # check .kv file
         self.add_widget(btn_hub)
         self.add_widget(img_logo)
 
