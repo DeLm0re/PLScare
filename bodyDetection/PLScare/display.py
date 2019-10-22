8fac0b1b9b43d8c087c68721ed489fccbdc6a567
RomainJacquiez <jacquiez.romain@orange.fr>
2019-10-22 09:07:30 +0200

    Clean up code and simplifications

--- bodyDetection/PLScare/display.py
+++ bodyDetection/PLScare/display.py
@@ -33,31 +33,14 @@ class LogoImg(Image):
 
 class SolutionImg(Image):
 
-    def __init__(self, **kwargs):
+    def __init__(self, path, x, y, **kwargs):
         super(SolutionImg, self).__init__(**kwargs)
 
         # param for the image
-        self.source = 'PLScare/assets/situations/arret/arretcardiaque1_medium-187.png'
+        self.source = path
         self.size_hint = (None, None)
         self.size_hint = 0.8, 0.8
-        self.pos_hint = {'center_x': 0.5, 'center_y': 0.7}
-
-
-class HubBtn(Button):
-
-    def __init__(self, **kwargs):
-        super(HubBtn, self).__init__(**kwargs)
-
-        # param for the button
-        self.text = 'Start recording'
-        self.size_hint = (None, None)
-        self.pos_hint = {'center_x': 0.5, 'center_y': 0.3}
-        self.font_name = 'PLScare/assets/fonts/OpenSans-Bold.ttf'
-        self.font_size = 50
-        self.color = 1, 0, 0, 1
-        self.padding = (30, 10)
-        self.background_normal = ''
-        self.background_down = 'PLScare/assets/images/grey.png'
+        self.pos_hint = {'center_x': x, 'center_y': y}
 
 
 class AskLabel(Label):
@@ -66,41 +49,24 @@ class AskLabel(Label):
         super(AskLabel, self).__init__(**kwargs)
 
         # param for the label
-        self.text = 'La victime présente elle des signes de saignements ?'
+        self.text = 'La victime présente-t-elle des signes de saignements ?'
         self.color = 1, 0, 0, 1
         self.font_name = 'PLScare/assets/fonts/OpenSans-Bold.ttf'
         self.font_size = 40
         self.pos_hint = {'center_x': 0.5, 'center_y': 0.8}
 
 
-class YesBtn(Button):
+class CustomBtn(Button):
 
-    def __init__(self, **kwargs):
-        super(YesBtn, self).__init__(**kwargs)
-
-        # param for the button
-        self.text = 'Oui'
-        self.size_hint = (None, None)
-        self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
-        self.font_name = 'PLScare/assets/fonts/OpenSans-Bold.ttf'
-        self.font_size = 40
-        self.color = 1, 0, 0, 1
-        self.padding = (30, 10)
-        self.background_normal = ''
-        self.background_down = 'PLScare/assets/images/grey.png'
-
-
-class NoBtn(Button):
-
-    def __init__(self, **kwargs):
-        super(NoBtn, self).__init__(**kwargs)
+    def __init__(self, x, y, text, size, **kwargs):
+        super(CustomBtn, self).__init__(**kwargs)
 
         # param for the button
-        self.text = 'Non'
+        self.text = text
         self.size_hint = (None, None)
-        self.pos_hint = {'center_x': 0.5, 'center_y': 0.2}
+        self.pos_hint = {'center_x': x, 'center_y': y}
         self.font_name = 'PLScare/assets/fonts/OpenSans-Bold.ttf'
-        self.font_size = 40
+        self.font_size = size
         self.color = 1, 0, 0, 1
         self.padding = (30, 10)
         self.background_normal = ''
@@ -113,7 +79,7 @@ class FirstScreen(Screen):
         super(Screen, self).__init__(**kwargs)
 
         # declaration des elements
-        btn_hub = HubBtn()
+        btn_hub = CustomBtn(0.5, 0.3, 'Enregistrer', 50)
         img_logo = LogoImg()
 
         # event on button
@@ -131,8 +97,12 @@ class SecondScreen(Screen):
 
         # declaration des elements
         ask_label = AskLabel()
-        yes_btn = YesBtn()
-        no_btn = NoBtn()
+        yes_btn = CustomBtn(0.5, 0.5, 'Oui', 40)
+        no_btn = CustomBtn(0.5, 0.2, 'Non', 40)
+
+        # event on button
+        yes_btn.bind(on_press=partial(switch_screen, self, '_third_screen_'))
+        no_btn.bind(on_press=partial(switch_screen, self, '_third_screen_'))
 
         # add element in screen
         self.add_widget(ask_label)
@@ -145,13 +115,16 @@ class ThirdScreen(Screen):
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
 
+        # declaration des elements
+        sol_img = SolutionImg('PLScare/assets/situations/arret/arretcardiaque1_medium-187.png', 0.1, 0.5)
+
+        # add element in screen
+        self.add_widget(sol_img)
+
 
 class PlscareApp(App):
 
     def build(self):
-        # init the screen size
-        init_window(600, 600)
-
         # init the screen manager
         screen_manager = ScreenManager()
         screen_manager.add_widget(FirstScreen(name='_first_screen_'))
@@ -161,4 +134,8 @@ class PlscareApp(App):
 
 
 def run_app():
+    # init the screen size
+    init_window(600, 600)
+
+    # run the app
     PlscareApp().run()
