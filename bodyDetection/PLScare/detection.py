bd5acb184c9f56c55c360974f18ea62fe9e70280
Louis <louis.schuck@gmail.com>
2019-10-22 09:34:06 +0200

    forehead detection and some cleaning

--- bodyDetection/PLScare/detection.py
+++ bodyDetection/PLScare/detection.py
@@ -27,8 +27,8 @@ def get_pose_id_closest_to_center(keypoint_scores, keypoint_coords, window_heigh
             list_center_gravity[pose_id][1] = np.average(coord[1])
             list_center_gravity[pose_id][0] = np.average(coord[0])
 
-    x_center = window_width/2
-    y_center = window_height/2
+    x_center = window_width / 2
+    y_center = window_height / 2
     list_dist_center = np.zeros(nb_pose)
     for pose_id in range(nb_pose):
         list_dist_center[pose_id] = \
@@ -63,6 +63,7 @@ def crop_image(square, image):
     return cropped_image
 
 
+# get the height of the face based on the eyes
 def get_height_face(x_left_eye, x_right_eye):
     distance_eyes = x_left_eye - x_right_eye
     height_face = math.pi * distance_eyes
@@ -70,25 +71,27 @@ def get_height_face(x_left_eye, x_right_eye):
     return height_face
 
 
+# Crop the body from a posenet skeleton
 def get_body(pose_id, keypoint_scores, keypoint_coords, image):
-        parts_names = ['leftShoulder', 'rightShoulder', 'leftHip', 'rightHip']
+    parts_names = ['leftShoulder', 'rightShoulder', 'leftHip', 'rightHip']
 
-        parts_info = get_info_skeleton(pose_id, keypoint_scores, keypoint_coords, parts_names)
+    parts_info = get_info_skeleton(pose_id, keypoint_scores, keypoint_coords, parts_names)
 
-        x_min = min(parts_info['leftShoulder']['x'], parts_info['rightShoulder']['x'],
-                    parts_info['leftHip']['x'], parts_info['rightHip']['x'])
-        x_max = max(parts_info['leftShoulder']['x'], parts_info['rightShoulder']['x'],
-                    parts_info['leftHip']['x'], parts_info['rightHip']['x'])
-        y_min = min(parts_info['leftShoulder']['y'], parts_info['rightShoulder']['y'],
-                    parts_info['leftHip']['y'], parts_info['rightHip']['y'])
-        y_max = max(parts_info['leftShoulder']['y'], parts_info['rightShoulder']['y'],
-                    parts_info['leftHip']['y'], parts_info['rightHip']['y'])
+    x_min = min(parts_info['leftShoulder']['x'], parts_info['rightShoulder']['x'],
+                parts_info['leftHip']['x'], parts_info['rightHip']['x'])
+    x_max = max(parts_info['leftShoulder']['x'], parts_info['rightShoulder']['x'],
+                parts_info['leftHip']['x'], parts_info['rightHip']['x'])
+    y_min = min(parts_info['leftShoulder']['y'], parts_info['rightShoulder']['y'],
+                parts_info['leftHip']['y'], parts_info['rightHip']['y'])
+    y_max = max(parts_info['leftShoulder']['y'], parts_info['rightShoulder']['y'],
+                parts_info['leftHip']['y'], parts_info['rightHip']['y'])
 
-        square = create_square(x_min, x_max, y_min, y_max, 1.3)
+    square = create_square(x_min, x_max, y_min, y_max, 1.3)
 
-        return crop_image(square, image)
+    return crop_image(square, image)
 
 
+# Crop the face from a posenet skeleton
 def get_face(pose_id, keypoint_scores, keypoint_coords, image):
     parts_names = ['leftEar', 'rightEar', 'leftEye', 'rightEye']
 
@@ -96,14 +99,32 @@ def get_face(pose_id, keypoint_scores, keypoint_coords, image):
 
     x_min = parts_info['rightEar']['x']
     x_max = parts_info['leftEar']['x']
-    y_min = parts_info['leftEye']['y']-get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
-    y_max = parts_info['leftEye']['y']+get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
+    y_min = parts_info['leftEye']['y'] - get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
+    y_max = parts_info['leftEye']['y'] + get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
 
     square = create_square(x_min, x_max, y_min, y_max, scale=1)
 
     return crop_image(square, image)
 
 
+# Crop the forehead person from a posenet skeleton
+def get_forehead(pose_id, keypoint_scores, keypoint_coords, image):
+    parts_names = ['leftEye', 'rightEye']
+    parts_info = get_info_skeleton(pose_id, keypoint_scores, keypoint_coords, parts_names)
+
+    face_height = get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x'])
+
+    top_forehead = parts_info['leftEye']['y'] - face_height / 2
+    bottom_forehead = max(parts_info['rightEye']['y'], parts_info['leftEye']['y'])
+    y_min = top_forehead + abs(bottom_forehead - top_forehead)/2
+    y_max = bottom_forehead - abs(bottom_forehead - top_forehead)/3
+    x_min = parts_info['rightEye']['x']
+    x_max = parts_info['leftEye']['x']
+    square = create_square(x_min, x_max, y_min, y_max, scale=1)
+    return crop_image(square, image)
+
+
+# Crop the full person from a posenet skeleton
 def get_person(pose_id, keypoint_scores, keypoint_coords, image):
     parts_names = ['nose', 'leftEye', 'rightEye', 'leftEar', 'rightEar', 'leftShoulder', 'rightShoulder',
                    'leftElbow', 'rightElbow', 'leftWrist', 'rightWrist', 'leftHip', 'rightHip', 'leftKnee',
@@ -121,6 +142,20 @@ def get_person(pose_id, keypoint_scores, keypoint_coords, image):
     return crop_image(square, image)
 
 
+# Crop the full person from a posenet skeleton
+def get_mouth(pose_id, keypoint_scores, keypoint_coords, image):
+    parts_names = ['nose', 'leftEar', 'rightEar', 'leftEye', 'rightEye']
+    parts_info = get_info_skeleton(pose_id, keypoint_scores, keypoint_coords, parts_names)
+
+    y_max = parts_info['leftEye']['y'] + get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
+    y_min = parts_info['nose']['y'] + np.abs(y_max - parts_info['nose']['y']) / 2
+    x_min = parts_info['rightEar']['x'] + np.abs(parts_info['leftEar']['x'] - parts_info['rightEar']['x']) / 3
+    x_max = parts_info['leftEar']['x'] - np.abs(parts_info['leftEar']['x'] - parts_info['rightEar']['x']) / 3
+    square = create_square(x_min, x_max, y_min, y_max, scale=1)
+    return crop_image(square, image)
+
+
+# Check if the hand is near the throat on a posenet skeleton
 def is_hand_near_throat(pose_id, keypoint_scores, keypoint_coords):
     # Extract all information used for the detection
     parts_names = ['leftShoulder', 'rightShoulder', 'leftElbow', 'rightElbow', 'leftWrist', 'rightWrist']
@@ -150,25 +185,15 @@ def is_hand_near_throat(pose_id, keypoint_scores, keypoint_coords):
     else:
         return False
 
-    if np.abs(y_shoulder-y_wrist) > np.abs(y_elbow-y_wrist):
+    if np.abs(y_shoulder - y_wrist) > np.abs(y_elbow - y_wrist):
         return False
 
     return True
 
 
+# Check if the mouth is open based on a posenet skeleton
 def is_mouth_open(pose_id, keypoint_scores, keypoint_coords, image):
-    parts_names = ['nose', 'leftEar', 'rightEar', 'leftEye', 'rightEye']
-    parts_info = get_info_skeleton(pose_id, keypoint_scores, keypoint_coords, parts_names)
-    x_left_ear = parts_info['leftEar']['x']
-    x_right_ear = parts_info['rightEar']['x']
-    y_nose = parts_info['nose']['y']
-
-    y_max = parts_info['leftEye']['y'] + get_height_face(parts_info['leftEye']['x'], parts_info['rightEye']['x']) / 2
-    y_min = y_nose + np.abs(y_max - y_nose)/2
-    x_min = x_right_ear + np.abs(x_left_ear - x_right_ear)/3
-    x_max = x_left_ear - np.abs(x_left_ear - x_right_ear)/3
-    square = create_square(x_min, x_max, y_min, y_max, scale=1)
-    image_mouth = crop_image(square, image)
+    image_mouth = get_mouth(pose_id, keypoint_scores, keypoint_coords, image)
 
     hsv = cv2.cvtColor(image_mouth, cv2.COLOR_BGR2HSV)
     average_value = np.average(hsv[:, :, 2])
@@ -182,6 +207,7 @@ def is_mouth_open(pose_id, keypoint_scores, keypoint_coords, image):
         return False
 
 
+# Get the different symptoms of a person based on a posenet skeleton
 def get_symptoms(keypoint_scores, keypoint_coords, image, window_height, window_width):
     symptoms = dict()
     pose_id = get_pose_id_closest_to_center(keypoint_scores, keypoint_coords, window_height, window_width)
@@ -195,30 +221,26 @@ def get_symptoms(keypoint_scores, keypoint_coords, image, window_height, window_
     return symptoms
 
 
-def get_diagnostic(symptoms):
+# Get the diagnostic of a person based on his symptoms
+# Each symptoms could be a percentage
+def get_diagnostics(symptoms):
     diagnostic = dict()
     diagnostic["Etouffement"] = \
-        symptoms["hand_near_throat"] + symptoms["mouth_open"] + \
-        (not symptoms["eyes_close"]) + \
-        symptoms["fast_cardiac_pace"] + \
-        (not symptoms["laying_on_ground"])
-
-    diagnostic["Saignement"] = \
-        (not symptoms["eyes_close"]) + \
-        symptoms["fast_cardiac_pace"] + \
-        1  # (whatever position)
+        (symptoms["hand_near_throat"] + symptoms["mouth_open"] + (1 - symptoms["eyes_close"]) +
+         symptoms["fast_cardiac_pace"] + (1 - symptoms["laying_on_ground"])) / 5
 
     diagnostic["Inconscient"] = \
-        symptoms["eyes_close"] + symptoms["laying_on_ground"] + \
-        (not symptoms["fast_cardiac_pace"] and not symptoms["no_cardiac_pace"])
+        (symptoms["eyes_close"] + symptoms["laying_on_ground"] +
+         (1 - symptoms["fast_cardiac_pace"] + 1 - symptoms["no_cardiac_pace"])) / 5
 
     diagnostic["Arret_cardiaque"] = \
-        (not symptoms["eyes_close"]) + symptoms["laying_on_ground"] + \
-        symptoms["no_cardiac_pace"]
+        ((1 - symptoms["eyes_close"]) + symptoms["laying_on_ground"] + symptoms["no_cardiac_pace"]) / 5
 
+    # To differentiate these two, ask the user if the person is hurt
     diagnostic["Malaise_cardiaque"] = \
-        (not symptoms["eyes_close"]) + 1 + 1  # (whatever position) (whatever cardiac pace)
+        ((1 - symptoms["eyes_close"]) + 1 + 1) / 5  # (+ 1 whatever position) (+ 1 whatever cardiac pace)
 
-    # si diagnostic["Saignement"] est le plus haut, demander confirmation
+    diagnostic["Saignement"] = \
+        ((1 - symptoms["eyes_close"]) + symptoms["fast_cardiac_pace"] + 1) / 5  # (+ 1 whatever position)
 
-    return diagnostic.index(max(diagnostic))
+    return diagnostic
